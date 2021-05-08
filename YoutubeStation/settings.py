468c6f91f65203564c2890621911335f8def26f3
Unit112358 <yunitto55@gmail.com>
2021-05-08 17:27:49 +0900

    Write test html

--- YoutubeStation/settings.py
+++ YoutubeStation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'live.apps.LiveConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
