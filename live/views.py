ce0e64fbbdfa8301c90731d4fb7e79632d876095
Unit112358 <yunitto55@gmail.com>
2021-05-08 17:31:43 +0900

    (^_^)

--- live/views.py
+++ live/views.py
@@ -3,5 +3,4 @@ from django.http import HttpResponse
 from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    # return render(request, 'live/index.html')
+    return render(request, 'live/index.html')
