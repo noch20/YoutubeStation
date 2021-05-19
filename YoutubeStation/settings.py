de1dc50458cbdad17e85df878b90dab5bcd36cac
Unit112358 <yunitto55@gmail.com>
2021-05-19 14:51:32 +0900

    Add home app

--- YoutubeStation/settings.py
+++ YoutubeStation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'live.apps.LiveConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- YoutubeStation/urls.py
+++ YoutubeStation/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('live.urls')),
+    path('', include('home.urls')),
+    path('live/', include('live.urls')),
     path('admin/', admin.site.urls),
 ]
